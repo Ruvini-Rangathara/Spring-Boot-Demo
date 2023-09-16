d72df7ad4beed8babb703c5505f4f053cb6bd841
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-09-16 16:45:03 +0530

    First commit

---
+++ src/main/java/com/example/springBootDemo/SBDemo/SbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootDemo.SBDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootDemo/SBDemo/SbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootDemo.SBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
